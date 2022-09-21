c0ec63ced7f440fa2e52b281852ce9c48b9adde1
alp <alperencantez@gmail.com>
2022-09-21 17:19:56 +0300

    add static files and templating

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  const blog = { id: 1, title: "Blog title", description: "Blog description" };
-  res.status(200).send(blog);
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/add", (req, res) => {
+  res.render("add");
+});
+
+app.get("/post", (req, res) => {
+  res.render("post");
 });
 
 app.listen(3000, () => console.log("Express is up and running!"));
